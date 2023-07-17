92f1a112d5d3e778b04d84ecf15c39fd428ff726
Pablo Arzu <jptomas38@gmail.com>
2023-07-17 10:41:44 -0600

    Primer commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState([])
+
+    const onAddCategory = (newCategory) => {
+
+        if( categories.includes( newCategory ) ) return;
+        setCategories( [ newCategory, ...categories  ] );
+    }
+
+  return (
+
+    <>
+        <h1>GiftExpertAp</h1>
+
+        <AddCategory onNewCategory={ onAddCategory } />
+
+        { 
+            categories.map( (category) => (
+                    <GifGrid 
+                        key={category} 
+                        category={category}
+                    />
+                ) ) 
+        }
+    </>
+
+  )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=tbL9rXYQdKk6UwrM9kCgrmwHkhsNkcub&q=${ category }&limit=10`
+  
+    const response = await fetch( url )
+    const { data } = await response.json()
+    
+    const gifs = data.map( img => ({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }));
+  
+     return gifs;
+  }
